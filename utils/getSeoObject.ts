ab3e3accb0a960ae4e27dcf8a9b156bedee6d8c3
AnhPhi <phananhphi.pp@gmail.com>
2023-02-16 23:20:40 +0700

    updadas

--- utils/getSeoObject.ts
+++ utils/getSeoObject.ts
@@ -2,11 +2,11 @@ import { get } from "lodash";
 
 const getSeoObject = (props: any, setting: any) => {
   // console.log("🚀 ~ file: getSeoObject.ts:4 ~ getSeoObject ~ setting", setting);
-  const title = undefined;
+  const title = "asdasdasdasdasdasdsaads";
   const description = get(props, "search_description");
   const image = undefined;
   const locale = get(props, "locale");
-  const company1111 = "asdasdasdadasdasdasdasdasd";
+  const company1111 = "1";
 
   return {
     title,
